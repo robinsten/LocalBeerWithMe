f4915d7e9dfad1f6df37e73b9a394f258df6c1e5
emeci <robin.stengel@mailbox.org>
2023-12-20 16:57:23 +0100

    Test

--- app/src/main/java/com/example/hmd/Cigarettes.java
+++ app/src/main/java/com/example/hmd/Cigarettes.java
@@ -1,28 +1,56 @@
 package com.example.hmd;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-
 public class Cigarettes extends AppCompatActivity {
+
+    private static final String PREFS_NAME = "MyPrefs";
+    private static final String SMOKED_CIGARETTES_KEY = "smokedCigarettes";
+
+    private SharedPreferences sharedPreferences;
+    private int smokedCigarettesCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cigarettes);
-        Button addOne = findViewById(R.id.addACigarette);
+
+
+        sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+
+
+        smokedCigarettesCount = sharedPreferences.getInt(SMOKED_CIGARETTES_KEY, 0);
+
+
         TextView Anzahl = findViewById(R.id.howManyCigarettes);
-        TextView Kosten = findViewById(R.id.CigarettesCost);
-        TextView Lebenszeit = findViewById(R.id.LostLifetime);
+        TextView CigarettesCost = findViewById(R.id.CigarettesCost);
+        TextView LostLifetime = findViewById(R.id.LostLifetime);
+        Anzahl.setText(String.valueOf(smokedCigarettesCount));
 
-        addOne.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        Button addOne = findViewById(R.id.addACigarette);
 
-            }
+        addOne.setOnClickListener(v -> {
+            smokedCigarettesCount++;
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putInt(SMOKED_CIGARETTES_KEY, smokedCigarettesCount);
+            editor.apply();
+
+            Anzahl.setText(String.valueOf(smokedCigarettesCount));
+           double centCosts = smokedCigarettesCount*35;
+           double lostHours = Math.floor(smokedCigarettesCount*16.67)/100;
+           double lostDays = Math.floor(lostHours*100/24)/100;
+            CigarettesCost.setText((centCosts/100) + "€ ");
+            LostLifetime.setText(smokedCigarettesCount * 10 + " min /     "
+                    + lostHours + " h /           "
+                    + lostDays + " Tage");
+            //test
+
         });
+
     }
+
+
 }
