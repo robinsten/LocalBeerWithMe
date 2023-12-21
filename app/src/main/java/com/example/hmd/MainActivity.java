a389308b4c0e39f7c81e9bd43613b9d0c6a5300c
emeci <robin.stengel@mailbox.org>
2023-12-21 23:25:49 +0100

    Snus nun fertig, programm funktioniert mit Tests

--- app/src/main/java/com/example/hmd/MainActivity.java
+++ app/src/main/java/com/example/hmd/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hmd;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 
@@ -29,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
         alkohol.setOnClickListener(v -> {
             Intent nextPage = new Intent(MainActivity.this, Alcohol.class);
             startActivity(nextPage);
+        });
+        snus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextPage = new Intent(MainActivity.this, Snus.class);
+                startActivity(nextPage);
+            }
         });
 
 
--- app/src/main/java/com/example/hmd/Snus.java
+++ app/src/main/java/com/example/hmd/Snus.java
@@ -1,4 +1,41 @@
 package com.example.hmd;
 
-public class Snus {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Snus  extends AppCompatActivity {
+
+    Button addOne;
+    private static final String PREFS_NAME = "MyPrefs";
+    private static final String SNUS_TAKEN_KEY = "SnusTaken";
+
+    private int SnusGesamt;
+
+    private SharedPreferences sharedPreferences;
+    TextView All;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.snus);
+        addOne = findViewById(R.id.addOneSnus);
+        All = findViewById(R.id.Snusoverview);
+        sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SnusGesamt = sharedPreferences.getInt(SNUS_TAKEN_KEY, 0);
+
+        addOne.setOnClickListener(v -> {
+            SnusGesamt++;
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putInt(SNUS_TAKEN_KEY, SnusGesamt);
+            editor.apply();
+            All.setText("Snus gesamt: " + SnusGesamt + "\n" + "Kosten ca.: " + SnusGesamt*23/100 + "€");
+
+        });
+    }
 }
